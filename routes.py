891ae51008b0d53a5efb023fddc6d36a7f061e82
Beben Perkasa <beben@localhost.localdomain>
2017-12-07 10:20:19 +0700

    page MAP

--- routes.py
+++ routes.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
 if __name__=='__main__':
     app.run(debug=True)
